149e0ef35fad917373fba32bdb48d60f4740146a
ashik113 <ashikcsbtech@gmail.com>
2023-11-11 10:01:56 -0800

    GPS detection is done as first thing

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: MaterialButton(
+          onPressed: () async {
+            //Get the current location
+            Position position = await Geolocator.getCurrentPosition(
+                desiredAccuracy: LocationAccuracy.low);
+            print(position);
+          },
+          child: const Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}
